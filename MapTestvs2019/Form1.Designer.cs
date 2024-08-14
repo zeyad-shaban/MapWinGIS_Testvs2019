fb66cdec2050b5a2d0fa1d9d6c1a35de5d887602
zeyad-shaban <zeyadshapan2004@gmail.com>
2024-08-14 16:19:31 +0300

    added layer for shape

---
+++ MapTestvs2019/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace MapTestvs2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.wgsBtn = new System.Windows.Forms.Button();
+            this.centeroctBtn = new System.Windows.Forms.Button();
+            this.centerHelwanBtn = new System.Windows.Forms.Button();
+            this.googleBtn = new System.Windows.Forms.Button();
+            this.axMap1 = new AxMapWinGIS.AxMap();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(913, 70);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MapTestvs2019.Properties.Resources.map;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(69, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(39)))));
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.wgsBtn);
+            this.panel3.Controls.Add(this.centeroctBtn);
+            this.panel3.Controls.Add(this.centerHelwanBtn);
+            this.panel3.Controls.Add(this.googleBtn);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 70);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(293, 433);
+            this.panel3.TabIndex = 2;
+            // 
+            // wgsBtn
+            // 
+            this.wgsBtn.Location = new System.Drawing.Point(91, 8);
+            this.wgsBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.wgsBtn.Name = "wgsBtn";
+            this.wgsBtn.Size = new System.Drawing.Size(69, 34);
+            this.wgsBtn.TabIndex = 9;
+            this.wgsBtn.Text = "WGS";
+            this.wgsBtn.UseVisualStyleBackColor = true;
+            this.wgsBtn.Click += new System.EventHandler(this.wgsBtn_Click);
+            // 
+            // centeroctBtn
+            // 
+            this.centeroctBtn.Location = new System.Drawing.Point(102, 45);
+            this.centeroctBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.centeroctBtn.Name = "centeroctBtn";
+            this.centeroctBtn.Size = new System.Drawing.Size(104, 65);
+            this.centeroctBtn.TabIndex = 8;
+            this.centeroctBtn.Text = "Center 6oct Airport";
+            this.centeroctBtn.UseVisualStyleBackColor = true;
+            this.centeroctBtn.Click += new System.EventHandler(this.centeroctBtn_Click);
+            // 
+            // centerHelwanBtn
+            // 
+            this.centerHelwanBtn.Location = new System.Drawing.Point(10, 45);
+            this.centerHelwanBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.centerHelwanBtn.Name = "centerHelwanBtn";
+            this.centerHelwanBtn.Size = new System.Drawing.Size(86, 65);
+            this.centerHelwanBtn.TabIndex = 7;
+            this.centerHelwanBtn.Text = "Center Helwan Airport";
+            this.centerHelwanBtn.UseVisualStyleBackColor = true;
+            this.centerHelwanBtn.Click += new System.EventHandler(this.centerHelwanBtn_Click);
+            // 
+            // googleBtn
+            // 
+            this.googleBtn.Location = new System.Drawing.Point(12, 8);
+            this.googleBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.googleBtn.Name = "googleBtn";
+            this.googleBtn.Size = new System.Drawing.Size(73, 34);
+            this.googleBtn.TabIndex = 5;
+            this.googleBtn.Text = "Google Map";
+            this.googleBtn.UseVisualStyleBackColor = true;
+            this.googleBtn.Click += new System.EventHandler(this.googleBtn_Click);
+            // 
+            // axMap1
+            // 
+            this.axMap1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMap1.Enabled = true;
+            this.axMap1.Location = new System.Drawing.Point(293, 70);
+            this.axMap1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.axMap1.Name = "axMap1";
+            this.axMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap1.OcxState")));
+            this.axMap1.Size = new System.Drawing.Size(620, 433);
+            this.axMap1.TabIndex = 3;
+            this.axMap1.MouseDownEvent += new AxMapWinGIS._DMapEvents_MouseDownEventHandler(this.axMap1_MouseDownEvent);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(39)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 35;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(275, 304);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(293, 446);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(620, 57);
+            this.panel1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 503);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.axMap1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private AxMapWinGIS.AxMap axMap1;
+        private System.Windows.Forms.Button googleBtn;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button centerHelwanBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button centeroctBtn;
+        private System.Windows.Forms.Button wgsBtn;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MapTestvs2019/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MapWinGIS;
+
+
+namespace MapTestvs2019
+{
+    public partial class Form1 : Form
+    {
+        private Shapefile _shape;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axMap1_MouseDownEvent(object sender, AxMapWinGIS._DMapEvents_MouseDownEvent e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void googleBtn_Click(object sender, EventArgs e)
+        {
+            axMap1.Projection = MapWinGIS.tkMapProjection.PROJECTION_GOOGLE_MERCATOR;
+        }
+
+        private void centerHelwanBtn_Click(object sender, EventArgs e)
+        {
+            // set to helwan runway
+            axMap1.Latitude = 29.8259655780143f;
+            axMap1.Longitude = 31.3282147049904f;
+        }
+
+        private void wgsBtn_Click(object sender, EventArgs e)
+        {
+            axMap1.Projection = MapWinGIS.tkMapProjection.PROJECTION_WGS84;
+        }
+
+        private void centeroctBtn_Click(object sender, EventArgs e)
+        {
+            // set to 6oct runway
+            axMap1.Latitude = 29.8146014f;
+            axMap1.Longitude = 30.8256197f;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GeoProjection proj = new GeoProjection();
+            proj.ImportFromEPSG(3857);
+
+            _shape = new Shapefile();
+            _shape.Open(@"C:\Users\zeyad\Desktop\Workspace\GIS\test.shp");
+            _shape.Reproject(proj, 1);
+
+            axMap1.AddLayer(_shape, true);
+        }
+    }
+}
